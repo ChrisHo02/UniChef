1eb06fb7f33b36400f2b291419f44692c21f65b6
twhc20 <twhc20@bath.ac.uk>
2021-02-16 16:05:24 +0000

    upload recipe

--- UniChef/app/src/main/java/com/example/unichef/MainActivity.java
+++ UniChef/app/src/main/java/com/example/unichef/MainActivity.java
@@ -1,6 +1,6 @@
 package com.example.unichef;
 
-import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,6 +15,8 @@ import androidx.navigation.ui.NavigationUI;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button uploadRecipe;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,20 +25,21 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications, R.id.navigation_settings)
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
 
-        Button button_delete_account = (Button) findViewById(R.id.button_delete);
-
-
-
+        uploadRecipe = (Button)findViewById(R.id.button);
+        uploadRecipe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,
+                        UploadRecipe.class);
+                startActivity(intent);
+            }
+        });
     }
 
-    //delete account onClick
-    public void deleteAccount(View view) {
-
-    }
 }
