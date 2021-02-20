77af7c6d8873e6932915bd14d770a72cf8aec906
Ben <bw687@bath.ac.uk>
2021-02-20 00:02:44 +0000

    Bug Fix

--- UniChef/app/src/main/java/com/example/unichef/MainActivity.java
+++ UniChef/app/src/main/java/com/example/unichef/MainActivity.java
@@ -7,9 +7,11 @@ import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
@@ -27,18 +29,16 @@ public class MainActivity extends AppCompatActivity {
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
                 .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        NavController navController = navHostFragment.getNavController();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
 
-        uploadRecipe = (Button)findViewById(R.id.button);
-        uploadRecipe.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this,
-                        UploadRecipe.class);
-                startActivity(intent);
-            }
+        uploadRecipe = findViewById(R.id.button);
+        uploadRecipe.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this,
+                    UploadRecipe.class);
+            startActivity(intent);
         });
 
     }
