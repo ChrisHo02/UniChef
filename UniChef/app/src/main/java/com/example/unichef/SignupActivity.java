43dc5df5c9b333425ff7f1249e2f46fbfa244cc4
Ben <bw687@bath.ac.uk>
2021-03-08 21:28:14 +0000

    Login and Registration now fully functional with Firebase

--- UniChef/app/src/main/java/com/example/unichef/SignupActivity.java
+++ UniChef/app/src/main/java/com/example/unichef/SignupActivity.java
@@ -2,16 +2,26 @@ package com.example.unichef;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.text.TextUtils;
 import android.util.Patterns;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.concurrent.atomic.AtomicBoolean;
 
@@ -23,6 +33,7 @@ public class SignupActivity extends AppCompatActivity {
     public EditText confirmPassword;
 
     public FirebaseAuth mAuth;
+    public DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,22 +47,23 @@ public class SignupActivity extends AppCompatActivity {
         confirmPassword = findViewById(R.id.confirmPassword);
 
         mAuth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance("https://unichef-f6056-default-rtdb.europe-west1.firebasedatabase.app/").getReference("users");
 
         register.setOnClickListener(v -> {
             if (checkValidData()){
-                mAuth.createUserWithEmailAndPassword(email.getText().toString(), password.getText().toString()).addOnCompleteListener(this, task -> {
-                    if (task.isSuccessful()){
-                        Intent intent = new Intent(SignupActivity.this, MainActivity.class);
-                        startActivity(intent);
-                    }else{
-                        try{
-                            throw task.getException();
-                        }catch (FirebaseAuthUserCollisionException e){
-                            email.setError("Email already in use!");
-                        }catch (Exception e){
-                            Toast.makeText(SignupActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                mDatabase.orderByChild("username").equalTo(username.getText().toString()).addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        if (!snapshot.exists()){
+                            registerUser();
+                        }else{
+                            username.setError("Username already exists");
+                            Toast.makeText(getApplicationContext(), "Check your info.", Toast.LENGTH_SHORT).show();
                         }
                     }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {}
                 });
             }else{
                 Toast.makeText(this, "Check your info.", Toast.LENGTH_SHORT).show();
@@ -67,10 +79,6 @@ public class SignupActivity extends AppCompatActivity {
         }
         if (isShort(username)){
             username.setError("Username is too short!");
-            valid = false;
-        }
-        if(usernameExists(username)){
-            username.setError("Username already exists!");
             valid = false;
         }
         if (isShort(password)){
@@ -94,7 +102,25 @@ public class SignupActivity extends AppCompatActivity {
         return textLength < 6;
     }
 
-    private boolean usernameExists(EditText text){
-        return false;
+    private void registerUser(){
+        mAuth.createUserWithEmailAndPassword(email.getText().toString(), password.getText().toString()).addOnCompleteListener(this, task -> {
+            if (task.isSuccessful()){
+                updateDatabase();
+                startActivity(new Intent(SignupActivity.this, MainActivity.class));
+            }else{
+                try{
+                    throw task.getException();
+                }catch (FirebaseAuthUserCollisionException e){
+                    email.setError("Email already in use!");
+                }catch (Exception e){
+                    Toast.makeText(SignupActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void updateDatabase(){
+        String newUserID = mAuth.getCurrentUser().getUid();
+        mDatabase.child(newUserID).child("username").setValue(username.getText().toString());
     }
 }
