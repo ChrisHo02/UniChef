775b2337d0b44c1708433ee5355d36623ef98fd2
Ben <bw687@bath.ac.uk>
2021-03-08 19:01:38 +0000

    Added firebase functionality to SignupActivity

--- UniChef/app/src/main/java/com/example/unichef/SignupActivity.java
+++ UniChef/app/src/main/java/com/example/unichef/SignupActivity.java
@@ -2,37 +2,99 @@ package com.example.unichef;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.text.TextUtils;
+import android.util.Patterns;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
 public class SignupActivity extends AppCompatActivity {
-    public Button button1;
-    public Button button2;
+    public Button register;
+    public EditText email;
+    public EditText username;
+    public EditText password;
+    public EditText confirmPassword;
+
+    public FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
-        button1 = (Button) findViewById(R.id.create_button);
-        button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(SignupActivity.this, MainActivity.class);
-                startActivity(intent);
-            }
-        });
+        register = findViewById(R.id.create_button);
+        email = findViewById(R.id.editTextEmail);
+        username = findViewById(R.id.editUsername);
+        password = findViewById(R.id.editPassword);
+        confirmPassword = findViewById(R.id.confirmPassword);
 
-        button2 = (Button) findViewById(R.id.back_button);
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
-                startActivity(intent);
+        mAuth = FirebaseAuth.getInstance();
+
+        register.setOnClickListener(v -> {
+            if (checkValidData()){
+                mAuth.createUserWithEmailAndPassword(email.getText().toString(), password.getText().toString()).addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()){
+                        Intent intent = new Intent(SignupActivity.this, MainActivity.class);
+                        startActivity(intent);
+                    }else{
+                        try{
+                            throw task.getException();
+                        }catch (FirebaseAuthUserCollisionException e){
+                            email.setError("Email already in use!");
+                        }catch (Exception e){
+                            Toast.makeText(SignupActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }else{
+                Toast.makeText(this, "Check your info.", Toast.LENGTH_SHORT).show();
             }
         });
     }
 
+    private boolean checkValidData(){
+        boolean valid = true;
+        if (!isEmail(email)){
+            email.setError("Enter valid email!");
+            valid = false;
+        }
+        if (isShort(username)){
+            username.setError("Username is too short!");
+            valid = false;
+        }
+        if(usernameExists(username)){
+            username.setError("Username already exists!");
+            valid = false;
+        }
+        if (isShort(password)){
+            password.setError("Password is too short!");
+            valid = false;
+        }
+        if(isShort(confirmPassword) || confirmPassword.getText().equals(password.getText())){
+            confirmPassword.setError("Passwords do not match");
+            valid = false;
+        }
+        return valid;
+    }
+
+    private boolean isEmail(EditText text){
+        CharSequence email = text.getText().toString();
+        return (!TextUtils.isEmpty(email) && Patterns.EMAIL_ADDRESS.matcher(email).matches());
+    }
+
+    private boolean isShort(EditText text){
+        int textLength = text.getText().toString().length();
+        return textLength < 6;
+    }
+
+    private boolean usernameExists(EditText text){
+        return false;
+    }
 }
